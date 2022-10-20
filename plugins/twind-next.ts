8d12c3a8ce843ef7b6f7c36690be0be63853d015
Richard <richard@stromer.org>
2022-10-20 21:11:05 +0200

    render page with twind next plugin and log result

--- plugins/twind-next.ts
+++ plugins/twind-next.ts
@@ -3,5 +3,10 @@ import { Plugin } from "../server.ts";
 export default function twind(): Plugin {
   return {
     name: "twind-next",
+    render(ctx) {
+      const res = ctx.render();
+      console.log(res);
+      return {};
+    },
   };
 }
