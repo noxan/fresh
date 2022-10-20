e67d0db3294e75a081274019e5195330e804b2aa
Richard <richard@stromer.org>
2022-10-20 21:49:16 +0200

    copy and add the preact tailwind class hook

--- plugins/twind-next.ts
+++ plugins/twind-next.ts
@@ -4,6 +4,39 @@ import {
   setup,
 } from "https://esm.sh/twind@1.0.0-next.39";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.0.0-next.39";
+import { JSX, options as preactOptions, VNode } from "preact";
+
+declare module "preact" {
+  namespace JSX {
+    interface DOMAttributes<Target extends EventTarget> {
+      class?: string;
+      className?: string;
+    }
+  }
+}
+
+const setupPreactTailwindHook = () => {
+  const originalHook = preactOptions.vnode;
+  // deno-lint-ignore no-explicit-any
+  preactOptions.vnode = (vnode: VNode<JSX.DOMAttributes<any>>) => {
+    if (typeof vnode.type === "string" && typeof vnode.props === "object") {
+      const { props } = vnode;
+      const classes: string[] = [];
+      if (props.class) {
+        classes.push(tw(props.class));
+        props.class = undefined;
+      }
+      if (props.className) {
+        classes.push(tw(props.className));
+      }
+      if (classes.length) {
+        props.class = classes.join(" ");
+      }
+    }
+
+    originalHook?.(vnode);
+  };
+};
 
 const STYLE_ELEMENT_ID = "__FRSH_TWIND";
 
@@ -15,6 +48,7 @@ const tw = setup(config);
 import { Plugin } from "../server.ts";
 
 export default function twind(): Plugin {
+  setupPreactTailwindHook();
   return {
     name: "twind-next",
     render(ctx) {
